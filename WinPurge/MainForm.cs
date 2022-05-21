a3d146014f5f6eaaedebabc7557db265936d6b84
TavSc <salascomparan.ao@outlook.com>
2022-05-21 00:54:29 -0500

    Created Additional Forms

--- WinPurge/MainForm.cs
+++ WinPurge/MainForm.cs
@@ -1,8 +1,8 @@
 namespace WinPurge
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
